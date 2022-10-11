e64cd04bead0784649a4bf4971c742897948baef
Letscat <43135049+Letscat@users.noreply.github.com>
2022-10-11 18:34:10 +0200

    Initial commit

---
+++ map.js
@@ -0,0 +1,281 @@
+
+
+
+
+// We specify the dimensions for the map container. We use the same
+// width and height as specified in the CSS above.
+let width = 900,
+    height = 600;
+
+// We define a letiable to later hold the data of the CSV.
+let mapData;
+
+// We get and prepare the Mustache template, parsing it speeds up future uses
+let template = d3.select('#template').html();
+Mustache.parse(template);
+
+// We create a SVG element in the map container and give it some
+// dimensions. We can use a viewbox and preserve the aspect ratio. This
+// also allows a responsive map which rescales and looks good even on
+// different screen sizes
+let svg = d3.select('#map').append('svg')
+  .attr("preserveAspectRatio", "xMidYMid")
+  .attr("viewBox", "0 0 " + width + " " + height);
+
+// We add a <g> element to the SVG element and give it a class to
+// style. We also add a class name for Colorbrewer.
+let mapFeatures = svg.append('g')
+  .attr('class', 'features statuscolor');
+
+// We add a <div> container for the tooltip, which is hidden by default.
+let tooltip = d3.select("#map")
+  .append("div")
+  .attr("class", "tooltip hidden");
+
+// Define the zoom and attach it to the map
+let zoom = d3.behavior.zoom()
+  .scaleExtent([1, 100])
+  .on('zoom', doZoom);
+svg.call(zoom);
+
+// We define a geographical projection
+//     https://github.com/mbostock/d3/wiki/Geo-Projections
+// and set some dummy initial scale. The correct scale, center and
+// translate parameters will be set once the features are loaded.
+let projection = d3.geo.mercator()
+  .scale(1);
+
+// We prepare a path object and apply the projection to it.
+let path = d3.geo.path()
+  .projection(projection);
+
+// We prepare an object to later have easier access to the data.
+let dataById = d3.map();
+
+
+
+//Categorize into 4 categorys
+let quantize = d3.scale.quantize()
+  .range(d3.range(4).map(function(i) { return 's' + i + '-4'; }));
+
+// We prepare a number format which will always return 2 decimal places.
+let formatNumber = d3.format('.2f');
+
+
+
+
+
+
+
+// Load the features from the GeoJSON.
+d3.json('data/ch2021g.geojson', function(error, features) {
+
+  // Get the scale and center parameters from the features.
+  let scaleCenter = calculateScaleCenter(features);
+
+  // Apply scale, center and translate parameters.
+  projection.scale(scaleCenter.scale)
+    .center(scaleCenter.center)
+    .translate([width/2, height/2]);
+
+  // Read the data for the cartogram
+  d3.csv('data/areastatistics.csv', function(data) {
+
+    // We store the data object in the letiable which is accessible from
+    // outside of this function.
+    mapData = data;
+
+    // This maps the data of the CSV so it can be easily accessed by
+    // the ID of the municipality, for example: dataById[2196]
+    dataById = d3.nest()
+      .key(function(d) { return d.id; })
+      .rollup(function(d) { return d[0]; })
+      .map(data);
+
+    // We add the features to the <g> element created before.
+    // D3 wants us to select the (non-existing) path objects first ...
+    mapFeatures.selectAll('path')
+        // ... and then enter the data. For each feature, a <path>
+        // element is added.
+        .data(features.features)
+      .enter().append('path')
+        // As "d" attribute, we set the path of the feature.
+        .attr('d', path)
+        // When the mouse moves over a feature, show the tooltip.
+        .on('mousemove', showTooltip)
+        // When the mouse moves out of a feature, hide the tooltip.
+        .on('mouseout', hideTooltip)
+        // When a feature is clicked, show the details of it.
+        .on('click', showDetails);
+
+    // Call the function to update the map colors.
+    updateMapColors();
+
+  });
+
+});
+
+/**
+ * Update the colors of the features on the map. Each feature is given a
+ * CSS class based on its value.
+ */
+function updateMapColors() {
+  // Set the domain of the values (the minimum and maximum values of
+  // all values of the current key) to the quantize scale.
+  quantize.domain([
+    d3.min(mapData, function(d) { return getValueOfData(d); }),
+    d3.max(mapData, function(d) { return getValueOfData(d); })
+  ]);
+  // Update the class (determining the color) of the features.
+  mapFeatures.selectAll('path')
+    .attr('class', function(f) {
+      // Use the quantized value for the class
+      return quantize(getValueOfData(dataById[getIdOfFeature(f)]));
+    });
+
+  // We call the function to update the legend.
+  updateLegend();
+}
+
+/**
+ * Show the details of a feature in the details <div> container.
+ * The content is rendered with a Mustache template.
+ *
+ * @param {object} f - A GeoJSON Feature object.
+ */
+function showDetails(f) {
+  // Get the ID of the feature.
+  let id = getIdOfFeature(f);
+  // Use the ID to get the data entry.
+  let d = dataById[id];
+
+  // Render the Mustache template with the data object and put the
+  // resulting HTML output in the details container.
+  let detailsHtml = Mustache.render(template, d);
+
+  // Hide the initial container.
+  d3.select('#initial').classed("hidden", true);
+
+  // Put the HTML output in the details container and show (unhide) it.
+  d3.select('#details').html(detailsHtml);
+  d3.select('#details').classed("hidden", false);
+}
+
+/**
+ * Hide the details <div> container and show the initial content instead.
+ */
+function hideDetails() {
+  // Hide the details
+  d3.select('#details').classed("hidden", true);
+  // Show the initial content
+  d3.select('#initial').classed("hidden", false);
+}
+
+/**
+ * Show a tooltip with the name of the feature.
+ *
+ * @param {object} f - A GeoJSON Feature object.
+ */
+function showTooltip(f) {
+  // Get the ID of the feature.
+  let id = getIdOfFeature(f);
+  // Use the ID to get the data entry.
+  let d = dataById[id];
+
+  // Get the current mouse position (as integer)
+  let mouse = d3.mouse(d3.select('#map').node()).map(
+    function(d) { return parseInt(d); }
+  );
+
+  // Calculate the absolute left and top offsets of the tooltip. If the
+  // mouse is close to the right border of the map, show the tooltip on
+  // the left.
+  let left = Math.min(width - 4 * d.name.length, mouse[0] + 5);
+  let top = mouse[1] + 25;
+
+  // Show the tooltip (unhide it) and set the name of the data entry.
+  // Set the position as calculated before.
+  tooltip.classed('hidden', false)
+    .attr("style", "left:" + left + "px; top:" + top + "px")
+
+    //here can the hover Infos be set
+    .html(d.name+" </br>"+ d.text+" </br>"+d.url);
+}
+
+/**
+ * Hide the tooltip.
+ */
+function hideTooltip() {
+  tooltip.classed('hidden', true);
+}
+
+/**
+ * Zoom the features on the map. This rescales the features on the map.
+ * Keep the stroke width proportional when zooming in.
+ */
+function doZoom() {
+  mapFeatures.attr("transform",
+    "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")")
+    // Keep the stroke width proportional. The initial stroke width
+    // (0.5) must match the one set in the CSS.
+    .style("stroke-width", 0.5 / d3.event.scale + "px");
+}
+
+/**
+ * Calculate the scale factor and the center coordinates of a GeoJSON
+ * FeatureCollection. For the calculation, the height and width of the
+ * map container is needed.
+ *
+ * Thanks to: http://stackoverflow.com/a/17067379/841644
+ *
+ * @param {object} features - A GeoJSON FeatureCollection object
+ *   containing a list of features.
+ *
+ * @return {object} An object containing the following attributes:
+ *   - scale: The calculated scale factor.
+ *   - center: A list of two coordinates marking the center.
+ */
+function calculateScaleCenter(features) {
+  // Get the bounding box of the paths (in pixels!) and calculate a
+  // scale factor based on the size of the bounding box and the map
+  // size.
+  let bbox_path = path.bounds(features),
+      scale = 0.95 / Math.max(
+        (bbox_path[1][0] - bbox_path[0][0]) / width,
+        (bbox_path[1][1] - bbox_path[0][1]) / height
+      );
+
+  // Get the bounding box of the features (in map units!) and use it
+  // to calculate the center of the features.
+  let bbox_feature = d3.geo.bounds(features),
+      center = [
+        (bbox_feature[1][0] + bbox_feature[0][0]) / 2,
+        (bbox_feature[1][1] + bbox_feature[0][1]) / 2];
+
+  return {
+    'scale': scale,
+    'center': center
+  };
+}
+
+/**
+ * Helper function to access the (current) value of a data object.
+ *
+ * Use "+" to convert text values to numbers.
+ *
+ * @param {object} d - A data object representing an entry (one line) of
+ * the data CSV.
+ */
+function getValueOfData(d) {
+  return +d['state'];
+}
+
+/**
+ * Helper function to retrieve the ID of a feature. The ID is found in
+ * the properties of the feature.
+ *
+ * @param {object} f - A GeoJSON Feature object.
+ */
+function getIdOfFeature(f) {
+  return f.properties.GMDNR;
+}
